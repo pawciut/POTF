957467f344a285730f0da18bb515d8bb5bdac682
DESKTOP-PEPNLPD\pawel <paweltruong@gmail.com>
2020-04-19 23:51:44 +0200

    grafiki i dalsze prace nad ekranem mapy

---
+++ POTF/Assets/Scripts/MapLogic.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MapLogic : MonoBehaviour
+{
+    public Button StartGameButton;
+    public Button NextTurnButton;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void StartGame()
+    {
+        StartGameButton.gameObject.SetActive(false);
+    }
+}

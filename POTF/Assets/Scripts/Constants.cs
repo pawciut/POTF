774a57c998051c1ab435963c5c9441b77288e470
Maksymilian Gembarzewski <maks.gembarzewski@gmail.com>
2020-04-19 20:04:02 +0200

    menu, credits, options zakończone :)

--- POTF/Assets/Scripts/Constants.cs
+++ POTF/Assets/Scripts/Constants.cs
@@ -4,7 +4,7 @@
     public const string MapSceneName = "MapScene";
     public const string Mission1SceneName = "MissionAScene";
     public const string MenuSceneName = "MenuScene";
-    public const string OptionSceneName = "MenuScene";
+    public const string OptionSceneName = "optionsScene";
     public const string CreditsSceneName = "CreditsScene";
     public const string HowToPlaySceneName = "HowToPlayScene";
 
